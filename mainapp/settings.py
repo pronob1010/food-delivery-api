75329324f84f3f128eca3d475447486b1182dddc
Pronob Mozumder <pronobmozumder.info@gmail.com>
2022-09-03 14:51:07 +0600

    secrets are added on env file:

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@n(-#!lp5slt_ah8@ot5-7#a0%7q(epb(m#41ndb!(p!x4$p2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cost=bool)
 
 ALLOWED_HOSTS = []
 
